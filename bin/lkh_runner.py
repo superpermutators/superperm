0b1c3aeae0374432d179880b8e03baa80e0347ce
Simon Lepasteur <simon.lepasteur@swissdotnet.ch>
2018-02-09 17:47:14 +0100

    Keep solutions according to weight + configurable number of runs.

--- bin/lkh_runner.py
+++ bin/lkh_runner.py
@@ -8,14 +8,16 @@ import subprocess
 
 
 def print_help():
-    print('Usage: lkh_runner.py -s LKH -o OUT_DIR -p PROBLEM -n NUMBER -m MAX_FILES')
+    print('Usage: lkh_runner.py -s LKH -o OUT_DIR -p PROBLEM -n NUMBER -w WEIGHT -r RUNS')
     print("    -s   Path to the lkh software. Default 'LKH'.")
     print("    -o   Output path. All lkh input files and output tour files are generated in this directory.\n"
           "         The directory is created if it does not exists. When running multiple instances it is\n"
           "         recommended to use different output directories.")
     print("    -p   Problem file. Default: 'atsp/6.atsp'.")
     print("    -n   The problem number. Default: '6'.")
-    print("    -m   Maximum number of file to keep. Default: '20'.")
+    print("    -w   Maximum weight of the solutions to keep. Default: '866'.")
+    print("    -r   The number of run executed by LKH on each iteration. Default: '50'."
+          "Greater means slightly better performance but some solutions might be discarded.")
     print("    -h   Display this help message.")
 
 
@@ -50,13 +52,14 @@ def get_args():
 
     lkh = myargs.get('-s', 'LKH')
     problem = myargs.get('-p', 'atsp/6.atsp')
-    max_files = myargs.get('-m', 20)
+    max_weight = myargs.get('-w', 866)
     number = myargs.get('-n', 6)
+    runs = myargs.get('-r', 50)
 
-    return lkh, directory, problem, int(max_files), int(number)
+    return lkh, directory, problem, int(max_weight), int(number), int(runs)
 
 
-def generate_input_file(directory, problem, seed, number):
+def generate_input_file(directory, problem, seed, number, runs):
     print(seed)
     filename = '{}/{}.par.{}'.format(directory, number, seed)
     file = open(filename, 'w')
@@ -64,7 +67,7 @@ def generate_input_file(directory, problem, seed, number):
         '''
 PROBLEM_FILE = {prob}
 OUTPUT_TOUR_FILE = {dir}/{n}.$.lkh.{seed}
-RUNS = 20
+RUNS = {runs}
 MAX_TRIALS = 3000
 BACKTRACKING = YES
 
@@ -73,29 +76,30 @@ MOVE_TYPE = 3
 PATCHING_C = 3
 PATCHING_A = 2
 SEED = {seed}
-EOF'''.format(dir=directory, prob=problem, seed=seed, n=number)
+EOF'''.format(dir=directory, prob=problem, seed=seed, n=number, runs=runs)
     )
     file.close()
     return filename
 
 
-def cleanup_results(directory, max_files):
-    files = []
+def cleanup_results(directory, max_weight):
     for file in os.listdir(directory):
-        files.append(file)
-    files.sort()
-    files.reverse()
-    to_remove = files[:-max_files]
-    for f in to_remove:
-        os.remove(os.path.join(directory, f))
+        file_parts = file.split('.')
+        if len(file_parts) == 4 and file_parts[2] == 'lkh':
+            if int(file_parts[1]) > max_weight:
+                print('Removing uninteresting solution file: {}\n'.format(file))
+                os.remove(os.path.join(directory, file))
+
+
+def main():
+    lkh, directory, problem, max_weight, number, runs = get_args()
+
+    while True:
+        input_file = generate_input_file(directory, problem, random.randrange(1, 2147483647), number, runs)
+        subprocess.call([lkh, input_file])
+        os.remove(input_file)
+        cleanup_results(directory, max_weight)
 
 
 if __name__ == '__main__':
-
-    lkh, directory, problem, max_files, number = get_args()
-
-    while True:
-        input_file = generate_input_file(directory, problem, random.randrange(1, 2147483647), number)
-        subprocess.call([lkh, input_file])
-        os.remove(input_file)
-        cleanup_results(directory, max_files)
+    main()
