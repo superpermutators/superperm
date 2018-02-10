b96e1ab6243d530e75ae542b901753f50630b600
Nabbo454 <33235324+Nabbo454@users.noreply.github.com>
2018-02-10 15:11:34 -0500

    Re-wrote a lot of the demutator, made it a bit more robust.

--- demutator/demutator.c
+++ demutator/demutator.c
@@ -1,8 +1,8 @@
-/* "EW, C!" */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
 struct permutable { /* super-important structure */;
 	int offset;
@@ -16,76 +16,115 @@ int factorial(int x) {
 		return x * factorial(x - 1);
 }
 
-char* formatplist(struct permutable *plist, int size, int radix, char* buffer, int length) {
-    /* variables
-     index -> which line of permutations are we on? (this is useful due to how this formats things)
-     i -> temporary index of *plist
-     a -> offset, used to make sure we print the correct # of spaces
-     c -> used to determine buffer offset, more like a "whole length" offset rather than a per-number offset
-     ptr -> current buffer pointer, used to poke things into the buffer.
+/** *plist -> list of found permutations
+	size -> how many there are
+	radix -> the radix thing
+	*buffer -> line buffer for file
+	length -> how long the line buffer is
+**/
+char* formatplist(struct permutable *plist, const int size, const uint8_t radix, char* buffer, const int length) {
+    /*	variables
+		index -> which line of permutations are we on? (this is useful due to how this formats things)
+		i -> temporary index of *plist
+		a -> offset between numbers
+		c -> used to determine buffer offset
+		ptr -> current buffer pointer, used to poke things into the buffer.
     */
-	int index = radix, i, a, c , ptr = 0;
+	int i, currptr, lineptr = 0;
+	uint8_t index;
+	/* fill the buffer with spaces */
+	for (i = 0; i < length-1; i++) {
+		buffer[i] = ' ';
+	} buffer[i] = 0;
+	printf("%d\n", size);
 
-nachste: /* goto-ing is going on */
-    c = ptr; /* save last secondary offset */
-	for (i = index - 1; i < size; i += radix) { /* flip through *plist at specific intervals */
-		for (a = (plist[i].offset + c) - ptr; a > 0; a--) {
-			buffer[ptr++] = ' ';
-			/* if we somehow iterate the pointer to a value that's not able to be buffered given its offset, quit */
-			if (ptr >= length) return NULL;
+    for (index = radix; index > 0; index--) {
+		for (i = index - 1; i <= size; i += radix) { /* flip through *plist at specific intervals */
+			currptr = plist[i].offset + lineptr;
+			printf("%d\t", i);
+
+			if ((currptr + radix) >= length) { /* if there isn't room in the buffer, quit. */
+				if ((plist[i].offset + 1) >= length) { /* how did you manage that?? */
+					puts("Something happened that should've been impossible..");
+					return NULL;
+				} else if ((plist[i].offset + 2) >= length) {
+					buffer[plist[i].offset + 1] = 0;
+					puts("No space for newline...");
+					return buffer;
+				}
+				puts("NO MORE ROOM!");
+				break;
+			}
+
+			strncpy(buffer + currptr, plist[i].permutation, radix);
 		}
-		ptr += radix; /* step ahead (radix) spaces to make room for the permutation we're going to add */
-		if (ptr >= length) return NULL; /* if there isn't room in the buffer, quit. */
-		strncpy(buffer + (ptr - radix), plist[i].permutation, radix);
+		puts("");
+		lineptr = currptr + radix;
+		buffer[lineptr++] = '\n';
 	}
-	buffer[ptr++] = '\n'; /* tack on newline */
-	if (ptr >= length) return NULL; /* if there isn't enough space for anything else, quit. */
-	if (--index) goto nachste; /* goto-ing commence! (until index is 0) */
-	buffer[ptr - 1] = 0; /* tack on zero byte to mark end of string */
+	puts("Finished with this one.");
 	return buffer;
 }
 
-void demutator(const char* super, const int size, const int radix, FILE* output_file) {
-    /* load up some constants
-     bm -> bitmask
-     len -> *buffer length
+/**	*super -> string with superperm in it
+	size -> how long it is
+	radix -> the radix thing
+	*output_file -> output file
+**/
+void demutator(const char* super, const int size, const uint8_t radix, FILE* output_file) {
+    /*	load up some constants
+		BM -> bitmask
+		BUFF_LEN -> *buffer length
     */
-	const int bm = (1 << radix) - 1, len = size*radix*3;
-	/* variables
-	 c -> temporary character
-	 *buffer -> line buffer (for output file)
-	 cbm -> changeable bitmask
-	 base -> base pointer for *super
-	 offset -> offset for *super + base
-	 mag -> temporary buffer for selecting bit from cbm
-	 *plist -> data structure which holds an offset and the permutation starting at the offset
+	const int BUFF_LEN = size*radix + 1;
+	const uint64_t BM = (1 << radix) - 1;
+	/*	variables
+		c -> temporary character
+		*buffer -> line buffer (for output file) should be bigger than we need.
+		base -> base pointer for *super
+		offset -> offset for *super + base
+		cbm -> changeable bitmask
+		bit_cbm -> temporary buffer for selecting bit from cbm
+		*plist -> data structure which holds an offset and the permutation starting at the offset
 	*/
-	int index = -1;
-	char c, *buffer = (char*)malloc(len);
-	register int cbm = 0, base = 0, offset = 0, mag = 0;
-	struct permutable *plist = (struct permutable*)malloc(size * sizeof(struct permutable));
+	int index = -1, ERR = 0, aug_size = (size+1)-radix;
+	char c, *buffer = (char*)malloc(BUFF_LEN); buffer[BUFF_LEN - 1] = 0;
+	register int base = 0, offset = 0;
+	register uint64_t cbm = 0, bit_cbm = 0;
+	struct permutable *plist = (struct permutable*)malloc(aug_size * sizeof(struct permutable));
 
 	/* go through *super and search for all permutations */
-	for (base = 0; base <= size*radix - radix; base++) {
-		cbm = bm;
-		for (offset = 0; offset < radix; offset++) {
+	for (base = 0; base <= aug_size; base++) { /* stops short of buffer length */
+		cbm = BM; /* setup flags */
+
+		for (offset = 0; offset < radix; offset++) { /* find a permutation */
 			c = super[base + offset];
 			/* make sure c is a numeral between 1 and the radix */
-			if ((c < '1')||(c > ('0' + radix))) break;
-			mag = (1 << (c - '1'));
-			if (((cbm ^= mag)&mag) || (c == 0)) break; /* check to make sure this digit wasn't already found */
+			if ((c < '1')||(c > ('0' + radix))) { ERR = -1; break; }
+
+			/* check to make sure this digit wasn't already found */
+			bit_cbm = (1 << (c - '1'));
+			if (((cbm ^= bit_cbm)&bit_cbm) || (c == 0)) break;
 		}
-		if (c == 0) break; /* if null byte, exit. */
+
+		if (ERR) break;
 		if (cbm == 0) { /* if bitmask is cleared, add permutation to *plist */
-			plist[++index] = (struct permutable){ base,  strncpy((char*)calloc(radix + 1, 1), super + base, radix) };
+			if (++index == aug_size) {
+				puts("PERMUTATION ARRAY FILLED");
+				break; /* exit if we've filled up the buffer */
+			}
+			plist[index] = (struct permutable){ base,  strncpy((char*)calloc(radix + 1, 1), super + base, radix) };
 		}
+		if ((c == 0) || ((base + offset) == size)) break; /* if null byte or end, exit. */
 	}
-	/* format the *plist into whatever and concat into the output file */
-	if (index == size - 1)
-		fprintf(output_file, "%s\n", formatplist(plist, size, radix, buffer, len));
-	else
+	if (ERR) {
         /* if there was an error, print the offset of *super where it occured */
-		fprintf(output_file, "\n\n%d\n\n", index);
+        printf("ERROR @ index: %d  size: %d  base: %d offset: %d\n", index, size, base, offset);
+		fprintf(output_file, "\n\nERROR @ index: %d  size: %d  base: %d  offset: %d\n\n", index, size, base, offset);
+	} else {
+		/* format the *plist into whatever and concat into the output file */
+		fprintf(output_file, "%s\n", formatplist(plist, index, radix, buffer, BUFF_LEN));
+	}
 
     /* free all structures */
 	for (; index >= 0; index--) free(plist[index].permutation);
@@ -96,8 +135,10 @@ void demutator(const char* super, const int size, const int radix, FILE* output_
 int main() {
 	FILE *pfile = NULL, *outfile = NULL;
 	char *linebuffer, *filebuffer;
-	int length, top, radix, size; /* based on machine, radix may be restricted to 8 or 16 (int size) */
-	register int a = 0, y = 0;
+	uint8_t radix;
+	unsigned short temp;
+	int length, top, size;
+	register int pointer = 0, y = 0;
 
 	/* Open data file */
 	pfile = fopen("data.txt", "r");
@@ -115,7 +156,8 @@ int main() {
 	}
 
 	/* first number contains radix */
-	fscanf(pfile, "%d", &radix);
+	fscanf(pfile, "%hu", &temp);
+	radix = temp;
 	size = factorial(radix);
 	/* offset in bytes from first number */
 	top = ftell(pfile) + 2;
@@ -141,10 +183,11 @@ int main() {
 	}
 
 	/* split buffer into lines and calculate permutations */
-	while (sscanf((char*)filebuffer + a, "%s", linebuffer) != EOF) {
-		demutator(linebuffer, size, radix, outfile);
+	while (sscanf((char*)filebuffer + pointer, "%s", linebuffer) != EOF) {
+		y = strlen(linebuffer);
+		demutator(linebuffer, y++, radix, outfile);
 		fprintf(outfile, "%s\n\n", linebuffer);
-		a += strlen(linebuffer) + 1;
+		pointer += y;
 	}
 
 	/* cleanup */
