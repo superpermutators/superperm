03f48e8c6456da03e37e8bb2ad762116507aac43
Miles Gould <miles@assyrian.org.uk>
2019-03-14 16:08:56 +0000

    Add --count option to bin/splitsuperperm.py
    It turned out that `sort -u` was the bottleneck! Moving the counting
    into the script shaves off around 0.5s out of 0.7s for an n=9
    superpermutation.

--- bin/splitsuperperm.py
+++ bin/splitsuperperm.py
@@ -2,17 +2,13 @@
 # -*- encoding: utf-8 -*-
 from __future__ import division
 
+import argparse
+import math
 import sys
 
 SYMBOLS = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-n_str = sys.argv[1]
-n = int(n_str)
-
-sorted_perm = list(SYMBOLS[:n])
-
-def split_superperm(superperm):
-    perms = set()
+def permutations(n, superperm):
     counts = [0 for i in range(n)]
     duplicates = 0
     for i in range(n):
@@ -20,8 +16,8 @@ def split_superperm(superperm):
         counts[j] += 1
         if counts[j] == 2:
             duplicates += 1
-    if duplicates == 0: perms.add(superperm[0:n])
-    else: print "..."
+    if duplicates == 0: yield superperm[0:n]
+    else: yield "..."
     for i in range(1, len(superperm) - n + 1):
         old_char = superperm[i-1]
         new_char = superperm[i+n-1]
@@ -35,11 +31,26 @@ def split_superperm(superperm):
                 duplicates -= 1
             if counts[new] == 2:
                 duplicates += 1
-        if duplicates == 0: perms.add(superperm[i : i + n])
-    print len(perms)
+        if duplicates == 0: yield superperm[i : i + n]
+        else: yield '...'
+
+def split_superperm(superperm, opts):
+    if opts.count:
+        perms = set(p for p in permutations(opts.n, superperm) if p != '...')
+        perm_count = len(perms)
+        expected = math.factorial(opts.n)
+        print '{}{}'.format(perm_count, '*' if perm_count == expected else '')
+    else:
+        for p in permutations(opts.n, superperm):
+            print p
+
+parser = argparse.ArgumentParser(description='Find permutations in a string')
+parser.add_argument('-c', '--count', action='store_true')
+parser.add_argument('n', type=int)
+opts = parser.parse_args()
 
 if len(sys.argv) > 2:
-    split_superperm(sys.argv[2])
+    split_superperm(sys.argv[2], opts)
 else:
-    split_superperm(sys.stdin.read().strip())
+    split_superperm(sys.stdin.read().strip(), opts)
 
