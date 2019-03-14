1517945d5b599c42011d96c150911a7cc5f1d264
Miles Gould <miles@assyrian.org.uk>
2019-03-14 14:56:41 +0000

    Faster, more complex algo for splitting superperms

--- bin/splitsuperperm.py
+++ bin/splitsuperperm.py
@@ -12,10 +12,29 @@ n = int(n_str)
 sorted_perm = list(SYMBOLS[:n])
 
 def split_superperm(superperm):
-    for i in range(len(superperm) - n + 1):
+    counts = {}
+    for i in range(n):
+        counts[sorted_perm[i]] = 0
+    duplicates = 0
+    for i in range(n):
+        counts[superperm[i]] += 1
+        if counts[superperm[i]] > 1:
+            duplicates += 1
+    if duplicates == 0: print superperm[0:n]
+    else: print "..."
+    for i in range(1, len(superperm) - n + 1):
+        old_char = superperm[i-1]
         p = superperm[i : i + n]
-        s = sorted(p)
-        if s == sorted_perm: print p
+        new_char = p[-1]
+        if old_char != new_char:
+            counts[old_char] -= 1
+            counts[new_char] += 1
+            # permutationness may have changed
+            if counts[old_char] == 1:
+                duplicates -= 1
+            if counts[new_char] == 2:
+                duplicates += 1
+        if duplicates == 0: print p
         else: print "..."
 
 if len(sys.argv) > 2:
