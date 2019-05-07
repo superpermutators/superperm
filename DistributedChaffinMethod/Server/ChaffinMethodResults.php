b853cfefa02a5ade8e97d53ce97cc48f5ce9caf5
Jay Pantone <jay.pantone@gmail.com>
2019-05-07 17:33:06 -0500

    bugs

--- DistributedChaffinMethod/Server/ChaffinMethodResults.php
+++ DistributedChaffinMethod/Server/ChaffinMethodResults.php
@@ -210,16 +210,20 @@ for ($n = $max_n; $n >= $min_n; $n--)
 
 		$res = $pdo->query("SELECT num_finished from num_finished_tasks");
 		if ($res->rowCount() != 0){
+			echo "<tr>\n";
 			$row = $res->fetch();
 			echo "<td class='center'>Finished</td>\n";
 			echo "<td class='right'>" . $row['num_finished'] . "</td>\n";
+			echo "</tr>\n";
 		}
 
 		$res = $pdo->query("SELECT num_redundant from num_redundant_tasks");
 		if ($res->rowCount() != 0){
+			echo "<tr>\n";
 			$row = $res->fetch();
 			echo "<td class='center'>Redundant</td>\n";
 			echo "<td class='right'>" . $row['num_redundant'] . "</td>\n";
+			echo "</tr>\n";
 		}
 		echo "</table>\n";
 		};
