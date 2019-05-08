4da5dddb2f1061511bdf7d4a365c7b30ffabcbaa
Jay Pantone <jay.pantone@gmail.com>
2019-05-07 22:22:54 -0500

    adds page caching

--- DistributedChaffinMethod/Server/ChaffinMethodResults.php
+++ DistributedChaffinMethod/Server/ChaffinMethodResults.php
@@ -1,3 +1,11 @@
+<?php
+	$seconds_to_cache = 60;
+	$ts = gmdate("D, d M Y H:i:s", time() + $seconds_to_cache) . " GMT";
+	header("Expires: $ts");
+	header("Pragma: cache");
+	header("Cache-Control: max-age=$seconds_to_cache");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
