8361e6dc889e678c4c408685492fa5dc675a7956
Katie Anderson <katieand@ntdev.microsoft.com>
2019-05-10 10:47:00 -0700

    typo fix

--- DistributedChaffinMethod/Debugging/curl.cmd
+++ DistributedChaffinMethod/Debugging/curl.cmd
@@ -1,5 +1,5 @@
 :: Simple wrapper that works like curl for windows users
 :: Note that it is fragile - it assumes the URL is in the third
 :: parameter and ignores the rest. This works with how URL_UTILITY
-:: is currently designed.
+:: is currently defined.
 @powershell -Command "(new-object net.webclient).DownloadString(\"%3\"")"
