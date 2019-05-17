55a75849f22629c2e3bbb414a77d7df1aca1fa4d
Greg Egan <gregegan@netspace.net.au>
2019-05-17 15:10:28 +0800

    Added links

--- DistributedChaffinMethod/Server/ChaffinMethodResults.php
+++ DistributedChaffinMethod/Server/ChaffinMethodResults.php
@@ -48,6 +48,14 @@ tr.active {background-color: #ddffdd;}
 </head>
 <body>
 <h1>Distributed Chaffin Method Results</h1>
+<p>The results shown below are from a <b>test server</b> that is trying out new software, which will soon be installed on the main server.
+Team Reykjavik <a href="http://ada.mscsnet.mu.edu/ChaffinMethodResults/">recently found</a> an optimal string for <i>w</i>=116, but it would be nice to independently confirm their result
+before moving on to the next stage. So if you have some spare CPU cores, join in the search!</p>
+<ul>
+<li>What is the <a href="https://github.com/superpermutators/superperm/wiki/Chaffin-method">Chaffin method</a>?</li>
+<li><a href="https://github.com/nagegerg/superperm/tree/master/DistributedChaffinMethod">GitHub repository</a> with the software you need to join this search. Please read the README!</li>
+<li>Sign up to the <a href="https://groups.google.com/forum/#!forum/superpermutators">Superpermutators</a> Google group to get the latest news and discuss the results.</li>
+</ul>
 
 <?php
 $dtz = new DateTimeZone(date_default_timezone_get());
