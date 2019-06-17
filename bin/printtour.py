c00f4a7f54f1d2e0df58ac5c53e17e5afe662b1b
Miles Gould <miles@assyrian.org.uk>
2019-06-17 17:45:36 +0100

    Add option to bin/printtour to fill in 1-cycles

--- bin/printtour.py
+++ bin/printtour.py
@@ -3,12 +3,17 @@
 from __future__ import division
 
 from itertools import permutations
-import sys
+import optparse
+
+parser = optparse.OptionParser(usage="%prog [options] N tour_filename")
+parser.add_option("-r", "--rotations", action="store_true",
+        help="fill in rotations of visited permutations")
+(options, args) = parser.parse_args()
 
 SYMBOLS = "123456789ABCDEFG"
 
-n = int(sys.argv[1])
-tour_filename = sys.argv[2]
+n = int(args[0])
+tour_filename = args[1]
 
 perms = list(permutations(range(n)))
 n_perms = len(perms)
@@ -24,6 +29,11 @@ def read_tour(tour_filename):
                 if ix == -1: break
                 yield "".join([ SYMBOLS[i] for i in perms[ix - 1] ])
 
+def rotations(xs):
+    for x in xs:
+        for i in range(n):
+            yield x[i:] + x[:i]
+
 def squash(xs):
     return reduce(lambda x, y: x + y[overlap(x, y):], xs, "")
 
@@ -33,4 +43,7 @@ def overlap(x, y):
         if x[len(x)-i:] == y[:i]:
             return i
 
-print squash(read_tour(tour_filename))
+tour = read_tour(tour_filename)
+if options.rotations:
+    tour = rotations(tour)
+print squash(tour)
