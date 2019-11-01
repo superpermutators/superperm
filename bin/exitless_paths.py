b2279a1ae8ba98fe16b5d6e2b8144b60525a2d41
Miles Gould <miles@assyrian.org.uk>
2019-11-01 12:41:29 +0000

    Add whitespace after commas

--- bin/exitless_paths.py
+++ bin/exitless_paths.py
@@ -21,14 +21,14 @@ def cycs(L1): #returns one-cycles of each vertex
 
 #searches exitless paths with at most cutoff one-cycles "skipped"
 #(one "skips" a one-cycle by using a 3-edge before fully doing a 2-cycle)
-def get(S,E,cutoff):
+def get(S, E, cutoff):
     todo = {}   #keeps track of the paths we need to check out, and try to continue further.
                 #the key corresponds to the current end of the path, and the value of the key is the set of such paths with that end
     done = {}   #keeps track of current least wastage to get to reach states.
                 #it's key is also the end of the path, which yeilds another dicitonary
                 #the second dicitonary is we give a certain wastage, which returns the set of states with that end which are known to have this wastage
 
-    for start,cost in S: #builds todo and done
+    for start, cost in S: #builds todo and done
         if cost > cutoff:
             continue
         end = start.pop(-1) #we always pop off our last vertex, that's where we start when we continue our path
@@ -41,21 +41,21 @@ def get(S,E,cutoff):
     queue = []
     while todo:
         for end in todo: 
-            queue.append((end,todo[end]))
+            queue.append((end, todo[end]))
         todo = {}
 
         while queue:
-            front,options = queue.pop(-1)
+            front, options = queue.pop(-1)
             for cost0 in done[front]:
                 ops = options&done[front][cost0]
                 options -= ops
                 #it would be nice to discard the part of ops which has already been checked with the specific front but I haven't gotten aroudn to it.
 
-                for (e,cost1) in E:
+                for (e, cost1) in E:
                     if cost0+cost1 > cutoff:
                         continue
-                    tail = add(e,front)
-                    poss = build(ops,cycs(tail[:-1]))
+                    tail = add(e, front)
+                    poss = build(ops, cycs(tail[:-1]))
 
                     if poss:
                         if tail[-1] not in done:
@@ -78,7 +78,7 @@ def findbests(done): #returns the length of the longest path of each 1-cycle ski
             print(i, best[i])
 
 
-def build(S,e): #find the paths in S which don't intersect with e
+def build(S, e): #find the paths in S which don't intersect with e
     return {path|e for path in S if not path&e}
 
 def add(e, front): #we start are path at front, and then move places according to e
@@ -89,12 +89,12 @@ def add(e, front): #we start are path at front, and then move places according t
 
 
 def phi(m): #represents a move as a tuple of indices
-    return tuple(idem[move(p,m)] for p in L)
+    return tuple(idem[move(p, m)] for p in L)
 
 
 #m is the indices you move around with your edge
 #since we always complete our 1-cycles in exitless paths, the last letter of your permutation will end up as the first letter when we're actually doing this move.
-def move(p,m):
+def move(p, m):
     x = list(p)
     y = []
     for i in m:
@@ -115,19 +115,19 @@ def genmoves(n): #create all n-edges
     return [reduce(a) for a in it.permutations(list(range(n-1))+[-1])]
 
 
-tau = phi([0,-1]) #2-edge
+tau = phi([0, -1]) #2-edge
 
-Ts = [([0],k-2)] #how we start our path, before our first 3-edge. ([visited vertices], defined wastage)
+Ts = [([0], k-2)] #how we start our path, before our first 3-edge. ([visited vertices], defined wastage)
 for i in range(k-2):
-    Ts.append((Ts[-1][0]+[tau[Ts[-1][0][-1]]],Ts[-1][1]-1))
+    Ts.append((Ts[-1][0]+[tau[Ts[-1][0][-1]]], Ts[-1][1]-1))
 
 moves = []
 
 for m in genmoves(3): #how we will continue our path, consists of a 3-edge, followed by a number of 2-edges
-    toadd = [((phi(m),),k-2)] #(tuple of maps, defined wastage), we use the sequence of maps with the "add" function
+    toadd = [((phi(m),), k-2)] #(tuple of maps, defined wastage), we use the sequence of maps with the "add" function
     for _ in range(k-2):
-        toadd.append((toadd[-1][0]+(tau,),toadd[-1][1]-1))
-        if len(cycs(add(toadd[-1][0],0))) < len(toadd[-1][0])+1: #checks if we re-entered a 1-cycle
+        toadd.append((toadd[-1][0]+(tau,), toadd[-1][1]-1))
+        if len(cycs(add(toadd[-1][0], 0))) < len(toadd[-1][0])+1: #checks if we re-entered a 1-cycle
             toadd.pop(-1)
             break
     print(m, phi(m)[0], len(toadd))
@@ -135,14 +135,14 @@ for m in genmoves(3): #how we will continue our path, consists of a 3-edge, foll
 
 #by the current definition of wastage I have, I'd say a 4-edge has an extra waste of (k-1), as we have double the cost (if we subtract the exprected cost of 2 for these being 2+-edges)
 '''for m in genmoves(4):
-    toadd = [((phi(m),tau),k-3+k-1)]
+    toadd = [((phi(m), tau), k-3+k-1)]
     for _ in range(k-3):
-        toadd.append((toadd[-1][0]+(tau,),toadd[-1][1]-1))
-        if len(cycs(add(toadd[-1][0],0))) < len(toadd[-1][0])+1:
+        toadd.append((toadd[-1][0]+(tau,), toadd[-1][1]-1))
+        if len(cycs(add(toadd[-1][0], 0))) < len(toadd[-1][0])+1:
             toadd.pop(-1)
             break
     print(m, phi(m)[0], len(toadd))
     moves += toadd'''
 
 if __name__ == '__main__':
-    get(Ts,moves,13)
+    get(Ts, moves, 13)
